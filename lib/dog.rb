a5caba5b2b0452b3dc3ae8e3f4aa5798321717f4
Henry Sztanski <henry@sztanski.com>
2019-04-25 03:04:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,6 +32,10 @@ class Dog
     self.new(id: row[0], name: row[1], breed: row[2])
   end
 
+  def self.find_or_create_by(name:, breed:)
+    
+  end
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs(
