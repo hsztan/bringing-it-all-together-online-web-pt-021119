3825da43ca72f81536ac3b31651cd1df2e38d7e5
Henry Sztanski <henry@sztanski.com>
2019-04-25 03:20:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,6 +46,13 @@ class Dog
     self.new(id: row[0], name: row[1], breed:  row[2])
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?
+      LIMIT 1
+      SQL
+  end
+
   def self.create_table
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS dogs(
