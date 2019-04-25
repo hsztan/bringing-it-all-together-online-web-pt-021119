f62bd7c382913b5b9662f547c78f89d2e5f33e7e
Henry Sztanski <henry@sztanski.com>
2019-04-25 03:13:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -38,7 +38,7 @@ class Dog
       dog_data = dog[0]
       dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      dog = self.create(name, breed)
+      dog = self.create(name: name, breed: breed)
     end
   end
 
